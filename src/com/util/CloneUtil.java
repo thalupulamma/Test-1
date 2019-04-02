9e5ab50ecf0526ec2bcfff96c5aa054414b3858b
thalupulamma <chandini412@gmail.com>
2019-04-02 15:49:27 +0530

    Clone Util added.

---
+++ src/com/util/CloneUtil.java
@@ -0,0 +1,25 @@
+package com.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.apache.log4j.Logger;
+
+public class CloneUtil {
+	private static final Logger log = Logger.getLogger(CloneUtil.class);
+	public static Object deepClone(Object object) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
+			oos.writeObject(object);
+			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			return ois.readObject();
+		} catch (Exception e) {
+			log.error("Failed to execute deepClone", e);
+			return null;
+		}
+	}
+}
